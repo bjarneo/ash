7c68b24cfa7d1a29b43d2e11759384581e4fb278
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-07-27 21:47:00 +0200

    fix: comment

--- lua/ash/plugins/hlchunk.lua
+++ lua/ash/plugins/hlchunk.lua
@@ -1,13 +1,12 @@
 -- hlchunk highlights for Ash theme
-local M = {}
-
-function M.apply(colors)
-	require("hlchunk").setup({
-		chunk = {
-			style = colors.comment, -- Using colors.comment for a gray/white style
-		},
-	})
-end
-
-return M
-
+-- local M = {}
+--
+-- function M.apply(colors)
+-- 	require("hlchunk").setup({
+-- 		chunk = {
+-- 			style = colors.comment, -- Using colors.comment for a gray/white style
+-- 		},
+-- 	})
+-- end
+--
+-- return M
